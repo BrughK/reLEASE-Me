a85ee23067275118f7df16ee9091be2f33cb369f
Kasen's Desktop <kasenb1211@gmail.com>
2023-03-12 23:19:30 -0400

    added all routes and made blank pages

--- client/src/components/TestComp.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Beans = () => {
-  return <h1>saghdfashdfgashgdfjhasgdfgasjhd</h1>;
-};
-
-export default Beans;
---
+++ client/src/components/creatListing.js
@@ -0,0 +1,173 @@
+import React from "react";
+
+const newList = () => {
+  return (
+    <>
+      <body className="bg-main-yellow full-class">
+        <style type="text/css">{`.full-class{
+          height: 911px;
+        }`}</style>
+        <div className="mt-10 bg-pale-dark sm:mt-0">
+          <div className="md:grid md:grid-cols-3 md:gap-6">
+            <div className="mt-5 md:col-span-2 md:mt-0">
+              <form action="#" method="POST">
+                <div className="overflow-hidden shadow sm:rounded-md">
+                  <div className="bg-pale-dark px-4 py-5 sm:p-6">
+                    <div className="grid grid-cols-6 gap-6">
+                      <div className="col-span-6 sm:col-span-3">
+                        <label
+                          htmlFor="first-name"
+                          className="block text-sm font-medium leading-6 text-main-yellow"
+                        >
+                          First name
+                        </label>
+                        <input
+                          type="text"
+                          name="first-name"
+                          id="first-name"
+                          autoComplete="given-name"
+                          className="mt-2 block w-full rounded-md border-0 py-1.5 text-main-yellow shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                      </div>
+
+                      <div className="col-span-6 sm:col-span-3">
+                        <label
+                          htmlFor="last-name"
+                          className="block text-sm font-medium leading-6 text-main-yellow"
+                        >
+                          Last name
+                        </label>
+                        <input
+                          type="text"
+                          name="last-name"
+                          id="last-name"
+                          autoComplete="family-name"
+                          className="mt-2 block w-full rounded-md border-0 py-1.5 text-main-yellow shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                      </div>
+
+                      <div className="col-span-6 sm:col-span-4">
+                        <label
+                          htmlFor="email-address"
+                          className="block text-sm font-medium leading-6 text-main-yellow"
+                        >
+                          Email address
+                        </label>
+                        <input
+                          type="text"
+                          name="email-address"
+                          id="email-address"
+                          autoComplete="email"
+                          className="mt-2 block w-full rounded-md border-0 py-1.5 text-main-yellow shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                      </div>
+
+                      <div className="col-span-6 sm:col-span-3">
+                        <label
+                          htmlFor="country"
+                          className="block text-sm font-medium leading-6 text-main-yellow"
+                        >
+                          Country
+                        </label>
+                        <select
+                          id="country"
+                          name="country"
+                          autoComplete="country-name"
+                          className="mt-2 block w-full rounded-md border-0 bg-white py-1.5 text-main-dark shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        >
+                          <option>United States</option>
+                          <option>Canada</option>
+                          <option>Mexico</option>
+                        </select>
+                      </div>
+
+                      <div className="col-span-6">
+                        <label
+                          htmlFor="street-address"
+                          className="block text-sm font-medium leading-6 text-main-yellow"
+                        >
+                          Street address
+                        </label>
+                        <input
+                          type="text"
+                          name="street-address"
+                          id="street-address"
+                          autoComplete="street-address"
+                          className="mt-2 block w-full rounded-md border-0 py-1.5 text-main-yellow shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                      </div>
+
+                      <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                        <label
+                          htmlFor="city"
+                          className="block text-sm font-medium leading-6 text-main-yellow"
+                        >
+                          City
+                        </label>
+                        <input
+                          type="text"
+                          name="city"
+                          id="city"
+                          autoComplete="address-level2"
+                          className="mt-2 block w-full rounded-md border-0 py-1.5 text-main-yellow shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                      </div>
+
+                      <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label
+                          htmlFor="region"
+                          className="block text-sm font-medium leading-6 text-main-yellow"
+                        >
+                          State / Province
+                        </label>
+                        <input
+                          type="text"
+                          name="region"
+                          id="region"
+                          autoComplete="address-level1"
+                          className="mt-2 block w-full rounded-md border-0 py-1.5 text-main-yellow shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                      </div>
+
+                      <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label
+                          htmlFor="postal-code"
+                          className="block text-sm font-medium leading-6 text-main-yellow"
+                        >
+                          ZIP / Postal code
+                        </label>
+                        <input
+                          type="text"
+                          name="postal-code"
+                          id="postal-code"
+                          autoComplete="postal-code"
+                          className="mt-2 block w-full rounded-md border-0 py-1.5 text-main-yellow shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <button
+                      type="submit"
+                      className="inline-flex justify-center rounded-md bg-main-dark py-2 px-3 text-sm font-semibold text-main-yellow shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                    >
+                      Save
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </body>
+      {/* Spacer block thing */}
+      {/* <div className="hidden sm:block" aria-hidden="true">
+        <div className="py-5">
+          <div className="border-t border-gray-200" />
+        </div>
+      </div> */}
+    </>
+  );
+};
+
+export default newList;
