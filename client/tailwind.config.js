3768ce97a83c35ca9db15047a119c48eed9c5ae2
haku <jmhallmark123@gmail.com>
2023-03-13 16:55:48 -0400

    example listings added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -14,5 +14,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    // require("@tailwindcss/aspect-ratio"),
+  ],
 };
